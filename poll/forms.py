fe9bb67efb19075bfa4688523de6343d87ac0b08
Bet4Arrio <emanuelsa-rv@hotmail.com>
2024-10-24 20:46:24 -0300

    aaaaazaaa

--- poll/forms.py
+++ poll/forms.py
@@ -7,8 +7,12 @@ from .models import *
         
 class VoteForm(forms.Form):
     fantasy = forms.ChoiceField(
-        choices=[(fantasy.pk, fantasy.name) for fantasy in Fantasy.objects.all()],
+        required=True
         widget=forms.RadioSelect,
         label="Choose your favorite"
     )
     email = forms.EmailField(required=False, label="Seu Email", widget=forms.EmailInput(attrs={"placeholder": "seu@email.com", "class":"form-control"}))
+    
+    def __init__(self, *args, **kwargs):
+        super(forms.Form, self).__init__(*args, **kwargs)
+        self.fields["fantasy"].choices = [(fantasy.pk, fantasy.name) for fantasy in Fantasy.objects.all()]
