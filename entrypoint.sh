2fbd7e1aad68be2bb7798429b172e1708c11b69e
Bet4Arrio <emanuelsa-rv@hotmail.com>
2024-10-24 20:00:04 -0300

    hot fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput 
 
-gunicorn timesaverSys.wsgi -b 0.0.0.0:8050 --log-level debug
+gunicorn timesaverSys.wsgi -b 0.0.0.0:8051 --log-level debug
